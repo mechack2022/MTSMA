eb103b9fb513111909348cdb2056dc48c03fe369
mechack2022 <dtoms624@gmail.com>
2025-02-21 17:40:33 +0100

    security utility added

---
+++ src/main/java/com/sms/multitenantschool/Utils/SecurityUtils/SecurityUtil.java
@@ -0,0 +1,55 @@
+package com.sms.multitenantschool.Utils.SecurityUtils;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public final class SecurityUtil {
+    private SecurityUtil() {
+    }
+
+    public static Optional<String> getCurrentUserLogin() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(extractPrincipal(securityContext.getAuthentication()));
+    }
+
+    private static String extractPrincipal(Authentication authentication) {
+        if (authentication == null) {
+            return null;
+        } else if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails)authentication.getPrincipal();
+            return springSecurityUser.getUsername();
+        } else {
+            return authentication.getPrincipal() instanceof String ? (String)authentication.getPrincipal() : null;
+        }
+    }
+
+    public static Optional<String> getCurrentUserJWT() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(securityContext.getAuthentication()).filter((authentication) -> authentication.getCredentials() instanceof String).map((authentication) -> (String)authentication.getCredentials());
+    }
+
+    public static boolean isCurrentUserInRole(String authority) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean var2;
+        if (authentication != null) {
+            Stream var10000 = getAuthorities(authentication);
+            authority.getClass();
+            if (var10000.anyMatch(authority::equals)) {
+                var2 = true;
+                return var2;
+            }
+        }
+
+        var2 = false;
+        return var2;
+    }
+
+    private static Stream<String> getAuthorities(Authentication authentication) {
+        return authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority);
+    }
+}
